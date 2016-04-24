24ddb3822dd67cf0c7209b1a2ab25b8e13b8294a
Lucas Soulier <rinke2005@gmail.com>
2016-04-25 00:47:39 +0200

    Add mutations for periods

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -22,6 +22,25 @@ const mutations = {
     },
     ADDFUNDATIONS(state_, fundations) {
         state_.fundations.push(...fundations);
+    },
+    UPDATEPERIOD(state_, period, data) {
+        state_.periods.forEach((p, i) => {
+            if (p.id === period.id) { 
+                state_.periods[i] = Object.assign(state_.periods[i], data);
+            }
+        });
+    },
+    DELETEPERIOD(state_, period) {
+        let i = 0;
+        for (const p of state_.periods) {
+            if (p.id === period.id) {
+                break;
+            }
+
+            ++i;
+        }
+
+        state_.periods.splice(i, 1);
     }
 };
 
