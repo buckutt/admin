75e9ae6335af654034a18082585b1fd381134e59
Lucas Soulier <rinke2005@gmail.com>
2016-11-23 01:14:08 +0100

    feat(devices): disallow the administrator to display items from different events on a device (#113)

--- src/components/Devices.vue
+++ src/components/Devices.vue
@@ -1,97 +1,100 @@
 <template>
-    <div class="devices" v-if="currentEvent">
-        <div class="mdl-card mdl-shadow--2dp">
-            <h3>Equipements</h3>
-            <transition name="fade">
-                <div v-if="selectedDevice.name">
-                    <h5>Modifier {{ selectedDevice.name }}</h5>
-                    <form @submit.prevent="updateDevice(modDevice)">
-                        <mdl-textfield floating-label="Nom" v-model="modDevice.name"></mdl-textfield>
-                        <mdl-textfield floating-label="Intervalle de rafraichissement" v-model="modDevice.refreshInterval"></mdl-textfield>
-                        <mdl-switch v-model="modDevice.realtime" class="mdl-js-ripple-effect">Temps réel</mdl-switch><br />
-                        <mdl-switch v-model="modDevice.doubleValidation" class="mdl-js-ripple-effect">Revalidation post-achats</mdl-switch><br />
-                        <mdl-switch v-model="modDevice.alcohol" class="mdl-js-ripple-effect">Avertissement alcool</mdl-switch><br />
-                        <mdl-switch v-model="modDevice.showCategories" class="mdl-js-ripple-effect">Afficher les catégories</mdl-switch><br />
-                        <mdl-switch v-model="modDevice.showPicture" class="mdl-js-ripple-effect">Afficher l'image utilisateur</mdl-switch><br />
+    <div class="container">
+        <div class="devices" v-if="currentEvent">
+            <div class="mdl-card mdl-shadow--2dp">
+                <h3>Equipements</h3>
+                <transition name="fade">
+                    <div v-if="selectedDevice.name">
+                        <h5>Modifier {{ selectedDevice.name }}</h5>
+                        <form @submit.prevent="updateDevice(modDevice)">
+                            <mdl-textfield floating-label="Nom" v-model="modDevice.name"></mdl-textfield>
+                            <mdl-textfield floating-label="Intervalle de rafraichissement" v-model="modDevice.refreshInterval"></mdl-textfield>
+                            <mdl-switch v-model="modDevice.realtime" class="mdl-js-ripple-effect">Temps réel</mdl-switch><br />
+                            <mdl-switch v-model="modDevice.doubleValidation" class="mdl-js-ripple-effect">Revalidation post-achats</mdl-switch><br />
+                            <mdl-switch v-model="modDevice.alcohol" class="mdl-js-ripple-effect">Avertissement alcool</mdl-switch><br />
+                            <mdl-switch v-model="modDevice.showCategories" class="mdl-js-ripple-effect">Afficher les catégories</mdl-switch><br />
+                            <mdl-switch v-model="modDevice.showPicture" class="mdl-js-ripple-effect">Afficher l'image utilisateur</mdl-switch><br />
 
-                        <mdl-button colored raised>Modifier</mdl-button>
-                    </form>
-                    <br />
-                    <h5>Assigner l'équipement</h5>
-                    <br />
-                    <form @submit.prevent="createPeriodPoint(selectedDevice, inputPeriodPoint)">
-                        <mdl-select label="Point" id="point-select" v-model="selectedPoint" :options="pointOptions"></mdl-select>
-                        <mdl-select label="Période" id="period-select" v-model="selectedPeriod" :options="periodOptions"></mdl-select>
+                            <mdl-button colored raised>Modifier</mdl-button>
+                        </form>
+                        <br />
+                        <h5>Assigner l'équipement</h5>
+                        <br />
+                        <form @submit.prevent="createPeriodPoint(selectedDevice, inputPeriodPoint)">
+                            <mdl-select label="Point" id="point-select" v-model="selectedPoint" :options="pointOptions"></mdl-select>
+                            <mdl-select label="Période" id="period-select" v-model="selectedPeriod" :options="periodOptions"></mdl-select>
 
-                        <mdl-button colored raised>Ajouter</mdl-button>
-                    </form>
-                    <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
-                        <thead>
-                            <tr>
-                                <th class="mdl-data-table__cell--non-numeric">Point</th>
-                                <th class="mdl-data-table__cell--non-numeric">Période</th>
-                                <th class="mdl-data-table__cell--non-numeric">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="periodPoint in detailsDevice.periodPoints" v-show="periodPoint.period.Event_id == currentEvent.id">
-                                <td class="mdl-data-table__cell--non-numeric" v-if="periodPoint.point">{{ periodPoint.point.name }}</td>
-                                <td class="mdl-data-table__cell--non-numeric" v-else>Aucun</td>
-                                <td class="mdl-data-table__cell--non-numeric">{{ periodPoint.period.name }}</td>
-                                <td class="mdl-data-table__cell--non-numeric">
-                                    <mdl-button @click.native="removePeriodPoint(periodPoint)">Supprimer</mdl-button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <br />
-                    <mdl-button colored raised @click.native="goBack()">Retour</mdl-button>
-                </div>
-            </transition>
-            <transition name="fade">
-                <div v-if="!selectedDevice.name">
-                    <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
-                        <thead>
-                            <tr>
-                                <th class="mdl-data-table__cell--non-numeric">Equipement</th>
-                                <th class="mdl-data-table__cell--non-numeric">Temps réel</th>
-                                <th class="mdl-data-table__cell--non-numeric">Intervalle</th>
-                                <th class="mdl-data-table__cell--non-numeric">Revalidation</th>
-                                <th class="mdl-data-table__cell--non-numeric">Alcool</th>
-                                <th class="mdl-data-table__cell--non-numeric">Catégories</th>
-                                <th class="mdl-data-table__cell--non-numeric">Image</th>
-                                <th class="mdl-data-table__cell--non-numeric">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="device in devices">
-                                <td class="mdl-data-table__cell--non-numeric">{{ device.name }}</td>
-                                <td class="mdl-data-table__cell--non-numeric">
-                                    <mdl-checkbox v-model="device.realtime" disabled></mdl-checkbox>
-                                </td>
-                                <td class="mdl-data-table__cell--non-numeric">{{ device.refreshInterval }}</td>
-                                <td class="mdl-data-table__cell--non-numeric">
-                                    <mdl-checkbox v-model="device.doubleValidation" disabled></mdl-checkbox>
-                                </td>
-                                <td class="mdl-data-table__cell--non-numeric">
-                                    <mdl-checkbox v-model="device.alcohol" disabled></mdl-checkbox>
-                                </td>
-                                <td class="mdl-data-table__cell--non-numeric">
-                                    <mdl-checkbox v-model="device.showCategories" disabled></mdl-checkbox>
-                                </td>
-                                <td class="mdl-data-table__cell--non-numeric">
-                                    <mdl-checkbox v-model="device.showPicture" disabled></mdl-checkbox>
-                                </td>
-                                <td class="mdl-data-table__cell--non-numeric">
-                                    <mdl-button @click.native="editDevice(device)">Modifier</mdl-button>
-                                    <mdl-button @click.native="removeDevice(device)">Supprimer</mdl-button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </transition>
+                            <mdl-button colored raised>Ajouter</mdl-button>
+                        </form>
+                        <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+                            <thead>
+                                <tr>
+                                    <th class="mdl-data-table__cell--non-numeric">Point</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Période</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="periodPoint in detailsDevice.periodPoints" v-show="periodPoint.period.Event_id == currentEvent.id">
+                                    <td class="mdl-data-table__cell--non-numeric" v-if="periodPoint.point">{{ periodPoint.point.name }}</td>
+                                    <td class="mdl-data-table__cell--non-numeric" v-else>Aucun</td>
+                                    <td class="mdl-data-table__cell--non-numeric">{{ periodPoint.period.name }}</td>
+                                    <td class="mdl-data-table__cell--non-numeric">
+                                        <mdl-button @click.native="removePeriodPoint(periodPoint)">Supprimer</mdl-button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <br />
+                        <mdl-button colored raised @click.native="goBack()">Retour</mdl-button>
+                    </div>
+                </transition>
+                <transition name="fade">
+                    <div v-if="!selectedDevice.name">
+                        <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+                            <thead>
+                                <tr>
+                                    <th class="mdl-data-table__cell--non-numeric">Equipement</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Temps réel</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Intervalle</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Revalidation</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Alcool</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Catégories</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Image</th>
+                                    <th class="mdl-data-table__cell--non-numeric">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="device in devices">
+                                    <td class="mdl-data-table__cell--non-numeric">{{ device.name }}</td>
+                                    <td class="mdl-data-table__cell--non-numeric">
+                                        <mdl-checkbox v-model="device.realtime" disabled></mdl-checkbox>
+                                    </td>
+                                    <td class="mdl-data-table__cell--non-numeric">{{ device.refreshInterval }}</td>
+                                    <td class="mdl-data-table__cell--non-numeric">
+                                        <mdl-checkbox v-model="device.doubleValidation" disabled></mdl-checkbox>
+                                    </td>
+                                    <td class="mdl-data-table__cell--non-numeric">
+                                        <mdl-checkbox v-model="device.alcohol" disabled></mdl-checkbox>
+                                    </td>
+                                    <td class="mdl-data-table__cell--non-numeric">
+                                        <mdl-checkbox v-model="device.showCategories" disabled></mdl-checkbox>
+                                    </td>
+                                    <td class="mdl-data-table__cell--non-numeric">
+                                        <mdl-checkbox v-model="device.showPicture" disabled></mdl-checkbox>
+                                    </td>
+                                    <td class="mdl-data-table__cell--non-numeric">
+                                        <mdl-button @click.native="editDevice(device)">Modifier</mdl-button>
+                                        <mdl-button @click.native="removeDevice(device)">Supprimer</mdl-button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </transition>
+            </div>
         </div>
+        <mdl-snackbar display-on="snackfilter"></mdl-snackbar>
     </div>
 </template>
 
@@ -111,8 +114,8 @@ export default {
                 showPicture     : false
             },
             detailsDevice : {},
-            selectedPeriod: {},
-            selectedPoint : {}
+            selectedPeriod: null,
+            selectedPoint : null
         };
     },
 
@@ -135,7 +138,7 @@ export default {
             this.selectedDevice = device;
             this.modDevice      = JSON.parse(JSON.stringify(device));
             this.selectedPoint  = null;
-            this.selectedPeriod = this.$store.state.app.periods[0];
+            this.selectedPeriod = null;
 
 
             const embedDevices = encodeURIComponent(JSON.stringify({
@@ -156,6 +159,33 @@ export default {
                 });
         },
         createPeriodPoint(device, periodPoint) {
+            console.log(this.detailsDevice.periodPoints);
+            if (periodPoint.Point_id) {
+                const filteredEvents = this.detailsDevice.periodPoints
+                    .filter(pP => {
+                        if (!pP.point) {
+                            return false;
+                        }
+
+                        const samePoint = (periodPoint.Point_id == pP.point.id);
+                        const overlap   = ((periodPoint.period.start <= pP.period.end) && (periodPoint.period.end >= pP.period.start));
+
+                        return samePoint && overlap;
+                    })
+                    .map(pP => pP.period.Event_id);
+
+                if (filteredEvents.length > 0) {
+                    if (periodPoint.period.Event_id != filteredEvents[0]) {
+                        return this.$root.$emit('snackfilter', {
+                            message: 'Le point est déjà utilisé par un autre événement sur cette période',
+                            timeout: 2000
+                        });
+                    }
+                }
+            }
+
+            delete periodPoint.period;
+
             let currentPeriodPoint  = {};
             const embedPeriodPoint  = encodeURIComponent(JSON.stringify({
                 point : true,
@@ -197,7 +227,7 @@ export default {
             const point         = this.selectedPoint;
             const period        = this.selectedPeriod;
             this.selectedPoint  = null;
-            this.selectedPeriod = this.$store.state.app.periods[0];
+            this.selectedPeriod = null;
             if(!point) {
                 return {
                     Period_id: period.id
@@ -205,6 +235,7 @@ export default {
             }
             return {
                 Period_id: period.id,
+                period   : period,
                 Point_id : point.id
             }
         },
@@ -255,6 +286,10 @@ export default {
                 width: 100%;
                 white-space: normal;
             }
+        }
+
+        & + .mdl-snackbar {
+            margin-left: $sidebarWidth / 2;
         }
     }
 </style>
