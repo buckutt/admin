2746fa8a17b9872a01135c10d81ab6d4112a734f
Lucas Soulier <rinke2005@gmail.com>
2017-06-01 12:13:29 +0200

    feat: use modal to confirm (#182)

--- src/components/Confirm.vue
+++ src/components/Confirm.vue
@@ -1,20 +1,16 @@
 <template>
     <div class="b--inline">
-        <transition name="fade">
-            <mdl-button raised accent :disabled="confirm || disabled" @click.native="switchConfirm()">
-                <slot></slot>
-            </mdl-button>
-        </transition>
-        <transition name="fade">
-            <div v-show="confirm" class="b--inline">
-                <mdl-button mini-fab colored @click.native="validate()">
-                    <i class="material-icons">done</i>
-                </mdl-button>
-                <mdl-button mini-fab @click.native="switchConfirm()">
-                    <i class="material-icons">clear</i>
-                </mdl-button>
-            </div>
-        </transition>
+        <mdl-button raised accent :disabled="disabled" @click.native="$refs.confirm.open">
+            <slot></slot>
+        </mdl-button>
+
+        <mdl-dialog ref="confirm" title="Suppression">
+            <p>Êtes-vous sûr de vouloir effectuer cette action ?</p>
+            <template slot="actions">
+                <mdl-button primary @click.native="validate">Valider</mdl-button>
+                <mdl-button @click.native="$refs.confirm.close">Annuler</mdl-button>
+            </template>
+        </mdl-dialog>
     </div>
 </template>
 
@@ -27,20 +23,10 @@ export default {
         }
     },
 
-    data() {
-        return {
-            confirm: false
-        };
-    },
-
     methods: {
-        switchConfirm() {
-            this.confirm = !this.confirm;
-        },
-
         validate() {
-            this.switchConfirm();
             this.$emit('confirm');
+            this.$refs.confirm.close();
         }
     }
 };
